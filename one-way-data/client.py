565245708258d08a50a9d0f4aa393dd706ad65fd
theoguzdogan <oguzdogan12@hotmail.com>
2024-02-18 20:38:52 +0300

    ADD: continuous stream from client ADD: connection established message on client ADD: greacefully and unexpected connection loss handling on server

--- one-way-data/client.py
+++ one-way-data/client.py
@@ -4,12 +4,20 @@ from ipconfig import IP
 
 async def transmit():
     uri = f"ws://{IP}:8765"  # Adjust the URI based on your server's host and port
+    print(f"Connected to {uri}")
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                data = input("Enter data (or type 'exit' to stop): ")
 
-    async with websockets.connect(uri) as websocket:
-        data = input("Enter data: ")
-        
-        await websocket.send(data)
-        print(f'Client Sent: {data}')
-        
+                if data.lower() == 'exit':
+                    break
+
+                await websocket.send(data)
+                print(f'Client Sent: {data}')
+
+        except websockets.ConnectionClosedError:
+            print("Connection closed. Retrying...")
+
 if __name__ == "__main__":
     asyncio.run(transmit())
--- one-way-data/server.py
+++ one-way-data/server.py
@@ -2,13 +2,23 @@ import asyncio
 import websockets
 from ipconfig import IP
 
-async def receive(websocket):
-    data = await websocket.recv()
-    print(f'Server Received: {data}')
-    
+async def receive(websocket, path):
+    try:
+        data = await websocket.recv()
+        print(f'Server Received: {data}')
+    except websockets.ConnectionClosedOK:
+        print("Client connection closed gracefully")
+    except websockets.ConnectionClosedError:
+        print("Client connection closed unexpectedly")
+
 async def main():
-    async with websockets.serve(receive, IP, 8765):
-        await asyncio.Future() #Run forever
-        
+    server = await websockets.serve(receive, IP, 8765)
+    print(f"Server started. Listening on {IP}:8765")
+
+    try:
+        await server.wait_closed()  # Run forever
+    except KeyboardInterrupt:
+        print("Server shutting down...")
+
 if __name__ == "__main__":
     asyncio.run(main())
